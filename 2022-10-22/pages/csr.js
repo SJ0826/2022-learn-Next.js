9fa542e5e2a3983abeb9a7c0e4fcc147dda67796
sujin <ikosdu60@gmail.com>
2022-10-22 17:19:43 +0900

    [ADD] Next.js: Next.js 완전정복 pages

---
+++ 2022-10-22/pages/csr.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import SubLayout from '../components/SubLayout'
+import Layout from '../components/Layout';
+
+export default function CSR() {
+  const [time, setTime] = useState();
+
+  useEffect(() => {
+    console.log('client');
+    setTime(new Date().toISOString())
+  },[]);
+
+  return (
+      <>
+        <h1 className="title">
+          {time}
+        </h1>
+      </>
+  )
+}
+
+CSR.getLayout = function getLayout(page) {
+  return (
+    <Layout>
+      <SubLayout>{page}</SubLayout>
+    </Layout>
+  )
+}
