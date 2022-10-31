75ec5a06bd7cfd725b3cd01d7f31f48413687e65
sujin <ikosdu60@gmail.com>
2022-10-31 18:57:55 +0900

    [ADD] Next.js: Next.js 완전정복 커머스 콘텐츠 상세페이지

---
+++ 2022-10-31/[id]/edit.tsx
@@ -0,0 +1,149 @@
+import Carousel from 'nuka-carousel'
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import CustomEditor from '@components/Editor'
+import { useRouter } from 'next/router'
+import { convertFromRaw, convertToRaw, EditorState } from 'draft-js'
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1014/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1014/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1013/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1013/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1012/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1012/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1011/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1011/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1016/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1016/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/4v.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/4v.jpg',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/1.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/1.jpg',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/2.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/2.jpg',
+  },
+]
+
+export default function Products() {
+  const [index, setIndex] = useState(0)
+  const router = useRouter()
+  const { id: productId } = router.query
+  const [editorState, setEditorState] = useState<EditorState | undefined>(
+    undefined
+  )
+
+  useEffect(() => {
+    if (productId != null) {
+      fetch(`/api/get-product?id=${productId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.items.contents) {
+            setEditorState(
+              EditorState.createWithContent(
+                convertFromRaw(JSON.parse(data.items.contents))
+              )
+            )
+          } else {
+            setEditorState(EditorState.createEmpty())
+          }
+        })
+    }
+  }, [productId])
+
+  const handleSave = () => {
+    if (editorState) {
+      fetch(`/api/update-product`, {
+        method: 'POST',
+        body: JSON.stringify({
+          id: String(productId),
+          contents: JSON.stringify(
+            convertToRaw(editorState.getCurrentContent())
+          ),
+        }),
+      })
+        .then((res) => res.json())
+        .then(() => {
+          alert('Success')
+        })
+    }
+  }
+
+  // return <ImageGallery items={images} />
+  return (
+    <>
+      <Carousel
+        animation="fade"
+        autoplay
+        withoutControls
+        wrapAround
+        speed={10}
+        slideIndex={index}
+      >
+        {images.map((item) => (
+          <Image
+            key={item.original}
+            src={item.original}
+            alt="image"
+            width={1000}
+            height={600}
+            layout="responsive"
+          />
+        ))}
+      </Carousel>
+      <div style={{ display: 'flex' }}>
+        {images.map((item, idx) => (
+          <div key={idx} onClick={() => setIndex(idx)}>
+            <Image
+              src={item.original}
+              alt="image"
+              width={100}
+              height={60}
+              layout="responsive"
+            />
+          </div>
+        ))}
+      </div>
+      {editorState != null && (
+        <CustomEditor
+          editorState={editorState}
+          onEditorStateChange={setEditorState}
+          onSave={handleSave}
+        />
+      )}
+    </>
+  )
+}
